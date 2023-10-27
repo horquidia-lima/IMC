11b51eb5ac8da85dfdcddafad393813c7745a1cb
horquidia-lima <horquidia.a.lima@gmail.com>
2023-10-26 21:52:04 -0300

    fechando o modal pelo teclado

--- modal.js
+++ modal.js
@@ -15,3 +15,11 @@ export const Modal = {
 Modal.buttonClose.onclick = () => {
   Modal.close()
 }
+
+window.addEventListener('keydown', handleKeyDown)
+
+function handleKeyDown(event) {
+  if(event.key === 'Escape') {
+    Modal.close()
+  }
+}
